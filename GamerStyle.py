1783dbcdc41d6dbae3b75413425aa72c6fc7d624
johnoneil <oneil.john@gmail.com>
2012-12-09 01:52:11 -0800

    Refined gamer style overlay a little more. Now nick and msg in separate columns.

--- GamerStyle.py
+++ GamerStyle.py
@@ -19,38 +19,86 @@ import cairo
 import pango
 import pangocairo
 
+#class GamerIRCMessage(IRCMessage):
+#  def __init__(self,nick,vhost,message):
+#    IRCMessage.__init__(self,nick,vhost,message)
+#    self.layout = None
+#    self.
+
 class GamerStyle:
   def __init__(self):
     self.buffer = IRCMessageBuffer()
-    self.lastTimestamp = 0
   def Push(self, msg ):
     self.buffer.Push(msg)
   def OnDraw(self,ctx,width,height,timestamp,deltaT):
     pangoCtx = pangocairo.CairoContext(ctx)
-    layout = pangoCtx.create_layout()
-    layout.set_width( pango.SCALE * (3*width/8) )
-    layout.set_alignment(pango.ALIGN_LEFT)
-    layout.set_wrap(pango.WRAP_WORD_CHAR)
-    layout.set_font_description(pango.FontDescription("Arial 12"))
-    layout.set_indent(-1* pango.SCALE * 40 )
-    ul_x = width/16
-    ul_y = 2*height/3
-    totaltext = ""
+    #layout the messages as follows:
+    #nick column is 1/8 total screen width, right justified
+    #message column is 2/8 total screen width, left justified with indent
+    #top of messages is at 2/3 down the screen
+    #left border of messages is 1/16 across the screen
+    nick_ul_x = width/16
+    nick_ul_y = 2*height/3
+    nick_width = width/8
+    msg_width = 2*width/8
+    gutter_width = 10
+    gutter_height = 5
+    msg_ul_x = nick_ul_x + nick_width + gutter_width
+    msg_ul_y = nick_ul_y
+    
     for entry in self.buffer:
       nick = entry.nick
-      msg = entry.nick + ":" + entry.msg
-      totaltext = totaltext + msg + "\n"
+      msg = entry.msg
 
-    ctx.move_to( ul_x,ul_y )
-    attrs, text, accel = pango.parse_markup(totaltext)
-    layout.set_attributes( attrs )
-    layout.set_text( text )
-    pangoCtx.update_layout(layout)
-    (device_extents,logical_extents) = layout.get_pixel_extents()
-    ctx.set_source_rgba(0,0,0,0.4)
-    ctx.rectangle(ul_x,ul_y,device_extents[2],device_extents[3])
-    ctx.fill()
-    ctx.move_to( ul_x,ul_y )
-    ctx.set_source_rgb(1,1,0)
-    pangoCtx.show_layout(layout)
+      nicklayout = pangoCtx.create_layout()
+      nicklayout.set_width( pango.SCALE * nick_width )
+      nicklayout.set_alignment(pango.ALIGN_RIGHT)
+      nicklayout.set_wrap(pango.WRAP_WORD_CHAR)
+      nicklayout.set_font_description(pango.FontDescription("Arial 12"))
+      nickattrs, nicktext, nickaccel = pango.parse_markup(nick)
+      nicklayout.set_attributes( nickattrs )
+      nicklayout.set_text( nicktext )
+      pangoCtx.update_layout(nicklayout)
+      (nick_dextents,nick_lextents) = nicklayout.get_pixel_extents()
+
+      msglayout = pangoCtx.create_layout()
+      msglayout.set_width( pango.SCALE * msg_width )
+      msglayout.set_alignment(pango.ALIGN_LEFT)
+      msglayout.set_wrap(pango.WRAP_WORD_CHAR)
+      msglayout.set_font_description(pango.FontDescription("Arial 12"))
+      msglayout.set_indent( pango.SCALE * 25 )
+      msgattrs, msgtext, msgaccel = pango.parse_markup(msg)
+      msglayout.set_attributes( msgattrs )
+      msglayout.set_text( msgtext )
+      pangoCtx.update_layout(msglayout)
+      (msg_dextents,msg_lextents) = msglayout.get_pixel_extents()
+
+      #the height of the total entry is the greater of the nick and msg heights
+      total_height = 0
+      if( msg_dextents[3] > nick_dextents[3]):
+        total_height = msg_dextents[3]
+      else:
+        total_height = nick_dextents[3]
+    
+      #draw the nick entry
+      ctx.move_to( nick_ul_x,nick_ul_y )
+      ctx.set_source_rgba(0,0,0,0.4)
+      ctx.rectangle(nick_ul_x,nick_ul_y,nick_width,total_height)
+      ctx.fill()
+      ctx.move_to( nick_ul_x,nick_ul_y )
+      ctx.set_source_rgb(1,1,1)
+      pangoCtx.show_layout(nicklayout)
+
+      #and draw the msg
+      ctx.move_to( msg_ul_x,msg_ul_y )
+      ctx.set_source_rgba(0,0,0,0.4)
+      ctx.rectangle( msg_ul_x, msg_ul_y, msg_width, total_height)
+      ctx.fill()
+      ctx.move_to( msg_ul_x,msg_ul_y )
+      ctx.set_source_rgb(1,1,0)
+      pangoCtx.show_layout(msglayout)
+
+      nick_ul_y = nick_ul_y + total_height + gutter_height
+      msg_ul_y = nick_ul_y 
+
 
