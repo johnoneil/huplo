cf1fdcd2d8bbcc626784ca00285577cbef6beb02
johnoneil <oneil.john@gmail.com>
2012-12-16 23:50:41 -0800

    Added basic infrastructure for easily constructed animation queues. Not even slightly debugged yet.

--- AnimatedMessageQueue.py
+++ AnimatedMessageQueue.py
@@ -122,6 +122,135 @@ class AnimatedMessageQueEntry(object):
     ctx.set_source_rgba(self._color.r, self._color.g, self._color.b, self._color.a)
     ctx.show_text(self._text)
 
+class Animation(object):
+  """Basic animation queueable object. Top of a tree structure that describes
+    various actions that will be performed (updated) simultaneous to the 
+    current action ("Do X AND y") and what whill be performed AFTER the current
+    action is completed ("do X THEN Y").
+    Intended as a base class for various sorts of animations, simplest of which
+    is a Pause.
+  """
+  def __init__(self):
+    self._sibling = None
+    self._child = None
+
+  def UpdateSiblings(self, dt, complete):
+    """Basic implementation of update functionality (uptate siblings,
+      return child when this animation is complete, isolated in a
+      method so that derived implementations can reuse it easily.
+      :param dt: Floating point time in seconds elapsed since last update
+      :type dt: float.
+      :param complete: Is the current animation task complete?
+      :type complete: bool.
+      :returns a reference to the current object (self) or the next if
+      this animation is complete.
+    """
+    if(self._sibling is not None):
+      self._sibling = self._sibling.Update(dt)
+      return self
+    elif( complete is not True):
+      return self
+    else:
+      return self._child
+
+  def Update(self, dt):
+    """Update the animation.
+       This is meant to be called like:
+       >>>
+        if( myAnimation is not None):
+          myAnimation = myAnimation.Update(dt)
+       >>>
+       thus
+       using the state pattern, myAnimation will be assigned with the subsequent 
+       next animation after the initial action (and its siblings) are complete.
+       When the entire animation is complete myAnimation.Update(dt) returns None
+      :param dt: Time elapsed in seconds since last update
+      :type dt: float
+      :returns Animation object or None if the animation is complete
+    """
+    return UpdateSiblings(self, True)
+    
+
+  def And(self, sibling):
+    """Add a sibling (simultaneously updated) animation to the current
+    animation. The current animation will not complete until all siblings
+    are complete.
+    Note, this method returns the self instance, so we can 
+    chain calls:
+    >>>
+    animation1 = Animation().And(Animation()).And(Animation())
+    while( animation1 is not None):
+      animation1 = animation1.Update(1.0)
+    >>>
+    :param sibling: The next simultaneously updated animation object you
+    wish to add
+    :type sibling: Animation
+    :returns Last sibling in the current animation chain, so we can chain calls.
+    """
+    if( self._sibling is not None):
+      self._sibling.And(sibling)
+    else:
+      self._sibling = sibling
+    return self
+
+  def Then(self, child):
+    """
+    Add a subsequent ("child") animation to the current animation.
+    The idea is we call Update() on a given animation, and when the parent completes,
+    the child is returned from the Update() call. We therefore only need to keep track of
+    one referene to the animation chain.
+    >>>
+    animation1 = Animation().Then(Animation()).Then(Animation())
+    while( animation1 is not None):
+      animation1 = animation1.Update(1.0)
+    >>>
+    """
+    if( self._child is not None):
+      self._child.Then(child)
+    else:
+      self._child = child
+    return self
+      
+
+class Pause(Animation):
+  def __init__(self, timeSec):
+    self._timeSec(timeSec)
+    self._timeElapsed = 0.0
+  def Update(self, dt):
+    self._timeElapsed = self._timeElapsed + dt
+    return self.UpdateSibling(dt, (self._timeElapsed >= self._timeSec) )
+
+class MoveTo(Animation):
+  def __init__(self, obj, pos, time_seconds, tween_type = None):
+    self._obj = obj
+    self._pos = pos
+    self._time_seconds = time_seconds
+    self._tweener = pyTween.Tweener()
+    self._tween_type = self._tweener.LINEAR
+    if( tween_type is not None):
+      self._tween_type = tween_type
+    self._tween = pyTweener.Tween(self._obj, self._time_seconds, self._tween_type, None, None, 0.0)
+  
+  def Update(self, dt):
+    self._tween.Update(dt)
+    return self.UpdateSibling(dt, self._tween.complete )
+    
+
+class TextBox(object):
+  def __init__(self, text, pos, text_color = None, bg_color = None, font = None):
+    self._text = text
+    self._pos = pos
+    if text_color is None:
+      self._text_color = color.white()
+    else:
+      self._text_color = text_color
+    self._bg_color = bg_color
+    self._font = font
+    self._animation = None
+
+  def MoveTo(self, pos, time_seconds, tween_type = None):
+    return MoveTo(self, pos, time_seconds, tween_type)
+
 class AnimatedMessageQueue(list):
   def __init__(self,  x, y, length = 5):
     self._length = length
