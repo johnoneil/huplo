10fd09667db9bed5f787da4478fbeef14dadeef9
johnoneil <oneil.john@gmail.com>
2013-12-16 00:31:50 -0800

    Overlay client accepts command line msg.

--- overlay_client.py
+++ overlay_client.py
@@ -15,7 +15,7 @@ import argparse
   
 def main():
   parser = argparse.ArgumentParser(description='Pick up overlay commands from dbus.')
-  #parser.add_argument('infile', help='Input domain specific text description of C++ structures to generate.')
+  parser.add_argument('msg', help='message to send to overlay server.')
   parser.add_argument('-v','--verbose', help='Verbose operation. Print status messages during processing', action="store_true")
   args = parser.parse_args()
   bus = dbus.SessionBus()
@@ -27,41 +27,12 @@ def main():
 
     #get ahold of interface off remote object and make simple call
     overlay_iface = dbus.Interface(remote_object, "com.VideoOverlay.ChatInterface")
-    reply = overlay_iface.HelloWorld('Yet another hello world from client.')
+    reply = overlay_iface.HelloWorld(args.msg)
     print 'Reply from server is ' + reply
   
   except dbus.DBusException:
     print_exc()
     sys.exit(1)
-  '''
-    # you can either specify the dbus_interface in each call...
-    #hello_reply_list = remote_object.HelloWorld("Hello from example-client.py!",
-    #    dbus_interface = "com.VideoOverlay.ChatInterface")
-  
-  print (hello_reply_list)
-
-  # ... or create an Interface wrapper for the remote object
-  iface = dbus.Interface(remote_object, "com.VideoOverlay.ChatInterface")
-  hello_reply_tuple = iface.GetTuple()
-
-  print hello_reply_tuple
-
-  hello_reply_dict = iface.GetDict()
-
-  print hello_reply_dict
-
-  # D-Bus exceptions are mapped to Python exceptions
-  try:
-    iface.RaiseException()
-  except dbus.DBusException, e:
-    print str(e)
-
-  # introspection is automatically supported
-  print remote_object.Introspect(dbus_interface="org.freedesktop.DBus.Introspectable")
-
-  if sys.argv[1:] == ['--exit-service']:
-    iface.Exit()
-  '''
 
 if __name__ == '__main__':
   main()
