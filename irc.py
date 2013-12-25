388f61756a74062550eebd48a6749d8fed2bd142
johnoneil <oneil.john@gmail.com>
2013-12-24 23:32:02 -0800

    Added support to reset/reverse irc markup to pango and italic.

--- irc.py
+++ irc.py
@@ -66,6 +66,7 @@ def formatting_to_pango_markup(msg):
       self.underline = False
       self.fg_color = -1
       self.bg_color = -1
+      self.italic = False
       
     def __call__(self, match):
       '''
@@ -77,6 +78,12 @@ def formatting_to_pango_markup(msg):
         self.underline = False
         self.fg_color = -1
         self.bg_color = -1
+        self.italic = False
+
+      if match.groupdict()['reverse'] is not None:
+        c = self.bg_color
+        self.bg_color = self.fg_color
+        self.fg_color = c
 
       if match.groupdict()['bold'] is not None:
         self.bold = not self.bold
@@ -84,12 +91,18 @@ def formatting_to_pango_markup(msg):
       if match.groupdict()['underline'] is not None:
         self.underline = not self.underline
 
+      if match.groupdict()['italic'] is not None:
+        self.italic = not self.italic
+
       if match.groupdict()['color'] is not None:
         print str(match.groupdict())
         if match.groupdict()['fg'] is not None:
           self.fg_color = int(match.groupdict()['fg'])
-      if match.groupdict()['bg'] is not None:
+        if match.groupdict()['bg'] is not None:
           self.bg_color = int(match.groupdict()['bg'])
+        if match.groupdict()['fg'] is None and match.groupdict()['bg'] is None:
+          self.fg_color = -1
+          self.bg_color = -1
 
       output = ''
 
@@ -105,6 +118,9 @@ def formatting_to_pango_markup(msg):
       if self.underline:
         output += 'underline="single" '
 
+      if self.italic:
+        output += 'style="italic" '
+
       if self.fg_color >=0:
         output += 'color="' + color_code_to_X11(self.fg_color) + '" '
 
@@ -115,7 +131,10 @@ def formatting_to_pango_markup(msg):
 
       return output
 
-  regex = r'((?P<reset>\x0f)|(?P<underline>\x1f)|(?P<bold>\x02)|(?P<color>\x03(?P<fg>\d{1,2})?(,(?P<bg>\d{1,2}))?))+'
+  #TODO: it would be best to refactor this to match not only the IRC binary markup
+  #but also the text between this markup and the next instance/eol. That
+  #would be easier to turn into ideal pango (HTML-like) markup tags.
+  regex = r'((?P<reset>\x0f)|(?P<underline>\x1f)|(?P<bold>\x02)|(?P<italic>\x1d)|(?P<reverse>\x12)|(?P<color>\x03(?P<fg>\d{1,2})?(,(?P<bg>\d{1,2}))?))+'
   markup_transform = MarkupFunctor()
   msg = re.sub(regex, markup_transform, msg)
   if markup_transform.match_found:
