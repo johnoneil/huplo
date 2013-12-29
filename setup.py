49c764345afe9f8638c4f79c7ea4f536fe7ae23f
johnoneil <oneil.john@gmail.com>
2013-12-28 16:26:13 -0800

    update setup.py and included distributed scripts

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def readme():
 setup(name='huplo',
     version='0.1',
     description='Overlay text on a gstreamer video via dbus and cairo/pango.',
-    long_description = readme()
-classifiers=[
+    long_description = readme(),
+	classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Multimedia :: Video :: Display',
       ],
-    keywords = 'gstreamer video text overlay python telemetry hud cairo dbus pycairo pango'
+    keywords = 'gstreamer video text overlay python telemetry hud cairo dbus pycairo pango',
     url='https://github.com/johnoneil/huplo',
     author='John O\'Neil',
     author_email='oneil.john@gmail.com',
@@ -34,4 +34,12 @@ classifiers=[
         'jsonpickle',
         'argparse'
       ],
+    scripts=[
+	'bin/huplo-animated-irc-client',
+	'bin/huplo-set-list',
+	'bin/huplo-set-ticker',
+	'bin/huplo-irc-client',
+	'bin/huplo-set-text',
+	'bin/huplo-view-stream',
+	],
       zip_safe=False)
